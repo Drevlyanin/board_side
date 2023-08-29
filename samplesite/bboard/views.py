03d4a420dd0431fa86f54e64f09574b3904311f4
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-08-29 20:00:11 +0300

    Add files via upload

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,10 +1,6 @@
-from django.http import HttpResponse 
-from django.template import loader
-
-from .models import Bb 
+from django.shortcuts import render
+from .models import Bb
 
 def index(request):
-    template = loader.get_template('bboard/index.html')
-    bbs = Bb.objects.order_by('-published') 
-    context = {'bbs': bbs} 
-    return HttpResponse(template.render(context, request))
+    bbs = Bb.objects.order_by('-published')
+    return render(request, 'bboard/index.html', {'bbs': bbs})
