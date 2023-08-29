34952eb1ede77623824919410ffc225c196134fb
Drevlyanin <121987689+Drevlyanin@users.noreply.github.com>
2023-08-29 19:59:33 +0300

    Add files via upload

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse 
+from django.template import loader
 
 from .models import Bb 
 
 def index(request):
-    s = 'Список объявлений\r\n\r\n\r\n' 
-    for bb in Bb.objects.order_by('-published'):
-        s += bb.title + '\r\n' + bb.content + '\r\n\r\n' 
-    return HttpResponse(s, content_type='text/plain; charset=utf-8') 
+    template = loader.get_template('bboard/index.html')
+    bbs = Bb.objects.order_by('-published') 
+    context = {'bbs': bbs} 
+    return HttpResponse(template.render(context, request))
